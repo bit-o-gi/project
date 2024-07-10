65c842b27e8dfddc4bdde40f8dc2d86df38c63f2
jhpark <compjhh@ls-electric.com>
2024-07-10 17:27:55 +0900

    feat: controller 기능 작성 - BE

--- src/main/java/bitcampnc5/bit/api/controller/schedule/ScheduleController.java
+++ src/main/java/bitcampnc5/bit/api/controller/schedule/ScheduleController.java
@@ -6,6 +6,10 @@ import bitcampnc5.bit.domain.schedule.Schedule;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +20,33 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    @GetMapping("/api/v1/schedules/{id}")
+    public ResponseEntity<Schedule> getSchedule(@PathVariable Long id) {
+        Schedule schedule = scheduleService.getSchedule(id);
+
+        return ResponseEntity.ok(schedule);
+    }
 
     @PostMapping("/api/v1/schedules/new")
     public ResponseEntity<Schedule> createSchedule(@Valid @RequestBody ScheduleRequest scheduleRequest) {
         Schedule createdSchedule = scheduleService.save(scheduleRequest);
 
         return ResponseEntity.ok(createdSchedule);
+    }
+
+    @PatchMapping("/api/v1/schedules/{id}")
+    public ResponseEntity<Schedule> updateSchedule(@PathVariable Long id,
+                                                   @Valid @RequestBody ScheduleRequest scheduleRequest) {
+        Schedule updatedSchedule = scheduleService.patch(id, scheduleRequest);
+
+        return ResponseEntity.ok(updatedSchedule);
+    }
+
+    @DeleteMapping("/api/v1/schedules/{id}")
+    public ResponseEntity<Schedule> deleteSchedule(@PathVariable Long id) {
+        Schedule deletedSchedule = scheduleService.delete(id);
+
+        return ResponseEntity.ok(deletedSchedule);
     }
 
 }
