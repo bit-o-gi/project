7a99e51e0579cb510c20518958b02ce8363a6150
jhpark <compjhh@ls-electric.com>
2024-07-10 17:18:33 +0900

    feat: service crud 로직 작성 - BE

--- src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleService.java
+++ src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleService.java
@@ -1,7 +1,14 @@
 package bitcampnc5.bit.api.service.schedule;
 
 import bitcampnc5.bit.api.dto.schedule.ScheduleRequest;
+import bitcampnc5.bit.domain.schedule.Schedule;
 
 public interface ScheduleService {
-    public void save(ScheduleRequest scheduleRequest);
+    Schedule getSchedule(Long id);
+
+    Schedule save(ScheduleRequest scheduleRequest);
+
+    Schedule patch(Long id, ScheduleRequest scheduleRequest);
+
+    void delete(Long id);
 }
--- src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImpl.java
+++ src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImpl.java
@@ -4,6 +4,7 @@ import bitcampnc5.bit.api.dto.schedule.ScheduleRequest;
 import bitcampnc5.bit.api.repository.schedule.ScheduleRepository;
 import bitcampnc5.bit.domain.schedule.Schedule;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +14,34 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
 
-    public void save(ScheduleRequest scheduleRequest) {
-        LocalDateTime startDateTime = scheduleRequest.getStartDateTime();
-        LocalDateTime endDateTime = scheduleRequest.getEndDateTime();
+    public Schedule getSchedule(Long id) {
+        return scheduleRepository.findById(id).orElse(null);
+    }
 
+    public Schedule save(ScheduleRequest scheduleRequest) {
+        checkScheduleStartToEndValid(scheduleRequest.getStartDateTime(), scheduleRequest.getEndDateTime());
+
+        return scheduleRepository.save(scheduleRequest.toEntity());
+    }
+
+    public Schedule patch(Long id, ScheduleRequest scheduleRequest) {
+        Schedule schedule = scheduleRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 일정이 존재하지 않습니다."));
+        checkScheduleStartToEndValid(scheduleRequest.getStartDateTime(), schedule.getEndDateTime());
+
+        schedule.update(scheduleRequest);
+        Schedule updatedSchedule = scheduleRequest.toEntity();
+        return scheduleRepository.save(updatedSchedule);
+    }
+
+    public void delete(Long id) {
+
+        scheduleRepository.deleteById(id);
+    }
+
+    private void checkScheduleStartToEndValid(LocalDateTime startDateTime, LocalDateTime endDateTime) {
         if (startDateTime.isAfter(endDateTime)) {
             throw new IllegalArgumentException("시작 시간이 종료 시간보다 늦을 수 없습니다.");
         }
-
-        scheduleRepository.save(scheduleRequest.toEntity());
     }
 }
