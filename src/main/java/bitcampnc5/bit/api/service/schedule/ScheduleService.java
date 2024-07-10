abb717480bd902b036a644703ba15dd45746905f
jhpark <compjhh@ls-electric.com>
2024-07-10 14:43:00 +0900

    feat: 서비스 구현 및 리팩토링, 예외처리 (green -> refactor) - BE

--- src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleService.java
+++ src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleService.java
@@ -1,4 +1,7 @@
 package bitcampnc5.bit.api.service.schedule;
 
+import bitcampnc5.bit.api.dto.schedule.ScheduleRequest;
+
 public interface ScheduleService {
+    public void save(ScheduleRequest scheduleRequest);
 }
--- src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImpl.java
+++ src/main/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImpl.java
@@ -1,4 +1,26 @@
 package bitcampnc5.bit.api.service.schedule;
 
-public class ScheduleServiceImpl {
+import bitcampnc5.bit.api.dto.schedule.ScheduleRequest;
+import bitcampnc5.bit.api.repository.schedule.ScheduleRepository;
+import bitcampnc5.bit.domain.schedule.Schedule;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleServiceImpl implements ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    public void save(ScheduleRequest scheduleRequest) {
+        LocalDateTime startDateTime = scheduleRequest.getStartDateTime();
+        LocalDateTime endDateTime = scheduleRequest.getEndDateTime();
+
+        if (startDateTime.isAfter(endDateTime)) {
+            throw new IllegalArgumentException("시작 시간이 종료 시간보다 늦을 수 없습니다.");
+        }
+
+        scheduleRepository.save(scheduleRequest.toEntity());
+    }
 }
