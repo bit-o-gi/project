ad38638668ffb1a3f8a4b2fc4771fd717b71bda1
jhpark <compjhh@ls-electric.com>
2024-07-10 17:17:00 +0900

    test: patch 관련 테스트 코드 작성 - BE

--- src/test/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImplTest.java
+++ src/test/java/bitcampnc5/bit/api/service/schedule/ScheduleServiceImplTest.java
@@ -1,29 +1,20 @@
 package bitcampnc5.bit.api.service.schedule;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.tuple;
-import static org.hamcrest.Matchers.any;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import bitcampnc5.bit.api.dto.schedule.ScheduleRequest;
 import bitcampnc5.bit.api.repository.schedule.ScheduleRepository;
 import bitcampnc5.bit.domain.schedule.Schedule;
 import java.time.LocalDateTime;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(MockitoExtension.class)
 class ScheduleServiceImplTest {
@@ -33,6 +24,7 @@ class ScheduleServiceImplTest {
 
     @InjectMocks
     private ScheduleServiceImpl scheduleService;
+
 
     @DisplayName("유효한 일정을 기입 했을 때, 일정이 잘 저장되는지 확인한다.")
     @Test
@@ -48,7 +40,7 @@ class ScheduleServiceImplTest {
         then(scheduleRepository).should().save(ArgumentMatchers.any(Schedule.class));
     }
 
-    @DisplayName("시작 시간이 종료 시간보다 뒤일 경우 예외가 발생한다")
+    @DisplayName("시작 시간이 종료 시간보다 뒤일 경우 예외가 발생한다.")
     @Test
     void saveValidateTest() {
         // given
@@ -61,6 +53,38 @@ class ScheduleServiceImplTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시작 시간이 종료 시간보다 늦을 수 없습니다.");
     }
+
+    @DisplayName("업데이트 시 존재하지 않는 일정을 찾을 시 예외가 발생한다.")
+    @Test
+    void patchTestWithNotExistsIdTest() {
+        // given
+        Long id = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        ScheduleRequest scheduleRequestDto = createScheduleRequest("testId", "일정 제목", "일정 내용", now, now);
+
+        // when // then
+        assertThatThrownBy(() -> scheduleService.patch(id, scheduleRequestDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("해당 일정이 존재하지 않습니다.");
+
+    }
+
+    @DisplayName("업데이트 시 시작시간이 종료시간보다 늦은 경우 예외가 발생한다.")
+    @Test
+    void patchTestWithNotValidTimeTest() {
+        // given
+        Long id = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        ScheduleRequest scheduleRequestDto = createScheduleRequest("testId", "일정 제목", "일정 내용", now.plusSeconds(1), now);
+        when(scheduleRepository.findById(id)).thenReturn(
+                java.util.Optional.of(new Schedule("testId", "일정 제목", "일정 내용", now, now)));
+
+        // when // then
+        assertThatThrownBy(() -> scheduleService.patch(id, scheduleRequestDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시작 시간이 종료 시간보다 늦을 수 없습니다.");
+    }
+
 
     private ScheduleRequest createScheduleRequest(String userId, String title, String content,
                                                   LocalDateTime startDateTime,
