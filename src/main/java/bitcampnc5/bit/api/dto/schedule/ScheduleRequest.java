31a443ce0dd39e657fa55ee51024cfeb6c449e26
jhpark <compjhh@ls-electric.com>
2024-07-10 14:25:32 +0900

    feat: Entity에 Validation 추가 - BE

---
+++ src/main/java/bitcampnc5/bit/api/dto/schedule/ScheduleRequest.java
@@ -0,0 +1,49 @@
+package bitcampnc5.bit.api.dto.schedule;
+
+import bitcampnc5.bit.domain.schedule.Schedule;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ScheduleRequest {
+
+    @NotNull(message = "유저 아이디는 필수입니다.")
+    private String userId;
+
+    @NotNull(message = "제목은 필수입니다.")
+    private String title;
+
+    private String content;
+
+    @NotEmpty(message = "시작 일시는 필수입니다.")
+    private LocalDateTime startDateTime;
+
+    @NotEmpty(message = "종료 일시는 필수입니다.")
+    private LocalDateTime endDateTime;
+
+    @Builder
+    public ScheduleRequest(String userId, String title, String content, LocalDateTime startDateTime,
+                           LocalDateTime endDateTime) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
+    public Schedule toEntity() {
+        return Schedule.builder()
+                .userId(userId)
+                .title(title)
+                .content(content)
+                .startDateTime(startDateTime)
+                .endDateTime(endDateTime)
+                .build();
+    }
+
+}
