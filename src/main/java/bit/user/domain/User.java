7eba69b7c54f1df19fb376b3fba2aafaa5f739d1
ParkSeryu <pjh971212@gmail.com>
2024-08-14 13:37:07 +0900

    feat: 기본 User Domain 및 Entity 작성 - BE

---
+++ src/main/java/bit/user/domain/User.java
@@ -0,0 +1,35 @@
+package bit.user.domain;
+
+import bit.user.dto.UserDto;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class User {
+    private final Long id;
+    private final String email;
+    private final String nickName;
+    private final String gender;
+    private final String platform;
+    private final LocalDateTime registerDate;
+
+    @Builder
+    public User(Long id, String email, String nickName, String gender, String platform, LocalDateTime registerDate) {
+        this.id = id;
+        this.email = email;
+        this.nickName = nickName;
+        this.gender = gender;
+        this.platform = platform;
+        this.registerDate = registerDate;
+    }
+
+    public static User from(UserDto userDto) {
+        return User.builder()
+                .email(userDto.getEmail())
+                .nickName(userDto.getNickName())
+                .gender(userDto.getGender())
+                .platform(userDto.getPlatform())
+                .build();
+    }
+}
