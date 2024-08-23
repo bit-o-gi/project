a01bb30b0e3fafd6b9a58b0f6c53e04d599b78c6
DESKTOP-5D4CMLH\simbonggyo <bong94688@gmail.com>
2024-08-23 23:01:10 +0900

    feat : 기념일 dday 계산 메서드 추가 - BE

--- src/main/java/bit/anniversary/sevice/AnService.java
+++ src/main/java/bit/anniversary/sevice/AnService.java
@@ -1,16 +1,16 @@
 package bit.anniversary.sevice;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import bit.anniversary.dto.AnDto;
-import bit.anniversary.dto.AnReqDto;
+import bit.anniversary.dto.AnResDto;
 import bit.anniversary.repository.AnRepository;
-import bit.board.dto.BoardDto;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
@@ -18,36 +18,48 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AnService {
 
-	private final AnRepository anRepository;
+    private final AnRepository anRepository;
 
-	public AnDto saveAnniverSary(AnDto andto) {
-		return anRepository.save(andto.creatAnniversary()).creatAnniversary();
-	}
 
-	@Transactional
-	public AnDto updateAnniverSary(AnDto anDto) {
-		anRepository.findById(anDto.getId()).orElseThrow(EntityNotFoundException::new).update(anDto);
-		return anDto;
-	}
+    public AnDto saveAnniverSary(AnDto andto) {
+        return anRepository.save(andto.creatAnniversary()).creatAnniversary();
+    }
 
-	// TODO: 추후 flyway 도입으로 crud에서 r 빼고 flyway에 책임을 넘길예정입니다.
-	@Transactional
-	public AnDto deleteAnniverSary(AnDto anDto) {
-		anRepository.deleteById(anDto.getId());
-		return anDto;
-	}
-	@Transactional(readOnly = true)
-	public AnReqDto getAnniverSary(AnDto anDto) {
-		return AnReqDto.of(anRepository.findById(anDto.getId()).orElseThrow(EntityNotFoundException::new).creatAnniversary());
-	}
+    @Transactional
+    public AnDto updateAnniverSary(AnDto anDto) {
+        anRepository.findById(anDto.getId()).orElseThrow(EntityNotFoundException::new).update(anDto);
+        return anDto;
+    }
 
-	@Transactional(readOnly = true)
-	public List<AnReqDto> getAnniverSaryList() {
-		List<AnReqDto> anReqDtos = new ArrayList<>();
-		anRepository.findAll().forEach(entity -> {
-			anReqDtos.add(entity.creatAnniversary().createAnReqDto());
-		});
-		return anReqDtos;
-	}
+    // TODO: 추후 flyway 도입으로 crud에서 r 빼고 flyway에 책임을 넘길예정입니다.
+    @Transactional
+    public AnDto deleteAnniverSary(AnDto anDto) {
+        anRepository.deleteById(anDto.getId());
+        return anDto;
+    }
 
+    @Transactional(readOnly = true)
+    public AnResDto getAnniverSary(AnDto anDto) {
+        return AnResDto.of(anRepository.findById(anDto.getId()).orElseThrow(EntityNotFoundException::new).creatAnniversary());
+    }
+
+    @Transactional(readOnly = true)
+    public List<AnResDto> getAnniverSaryList() {
+        List<AnResDto> anReqDtos = new ArrayList<>();
+        anRepository.findAll().forEach(entity -> {
+            anReqDtos.add(entity.creatAnniversary().createAnReqDto());
+        });
+        return anReqDtos;
+    }
+
+    //	NOTE: 현재 시간 <-> Dday 몇일 남았는지 계산하는 메서드
+    private long getNowAnniverSary(LocalDateTime anniverSary) {
+        LocalDateTime now = LocalDateTime.now();
+        return ChronoUnit.DAYS.between(now, anniverSary);
+    }
+
+    // 기념일 찾기기능
+//    private AnResDto findAnniverSary(AnDto){
+//
+//    }
 }
