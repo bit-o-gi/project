f83ae3dc28be0ba96458785dfae6697862f06127
rappire <rappire@gmail.com>
2024-08-30 22:13:24 +0900

    feat: 스케줄 엔티티 추가

---
+++ src/main/java/bit/schedule_new/domain/Schedule.java
@@ -0,0 +1,44 @@
+package bit.schedule_new.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime startDateTime;
+
+    private LocalDateTime endDateTime;
+
+    @Builder
+    public Schedule(Long userId, String title, String content, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        checkStartEndDateTime(startDateTime, endDateTime);
+        this.userId = Objects.requireNonNull(userId);
+        this.title = Objects.requireNonNull(title);
+        this.content = Objects.requireNonNull(content);
+        this.startDateTime = Objects.requireNonNull(startDateTime);
+        this.endDateTime = Objects.requireNonNull(endDateTime);
+    }
+
+    private void checkStartEndDateTime(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        if (startDateTime.isBefore(endDateTime)) {
+            throw new IllegalArgumentException("시작 시간은 종료 시간보다 늦을 수 없습니다.");
+        }
+    }
+}
