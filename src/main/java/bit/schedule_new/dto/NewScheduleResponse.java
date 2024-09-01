77b47a5d459235ffeb1c72eec063dfeebeea7b8a
rappire <rappire@gmail.com>
2024-09-01 12:40:31 +0900

    feat: 스케줄 저장 서비스 추가

--- src/main/java/bit/schedule_new/dto/NewScheduleResponse.java
+++ src/main/java/bit/schedule_new/dto/NewScheduleResponse.java
@@ -1,12 +1,17 @@
 package bit.schedule_new.dto;
 
 import bit.schedule_new.domain.NewSchedule;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@EqualsAndHashCode
 @Getter
 public class NewScheduleResponse {
+    private Long id;
+
+    private Long userId;
 
     private String title;
 
@@ -17,6 +22,8 @@ public class NewScheduleResponse {
     private LocalDateTime endDateTime;
 
     public NewScheduleResponse(NewSchedule schedule) {
+        this.id = schedule.getId();
+        this.userId = schedule.getUserId();
         this.title = schedule.getTitle();
         this.content = schedule.getContent();
         this.startDateTime = schedule.getStartDateTime();
