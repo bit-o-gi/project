fee6d56b4f4191f3a7b908a970e2e9711e54fb20
ParkSeryu <pjh971212@gmail.com>
2024-09-05 14:02:27 +0900

    refactor: exception 관련 핸들링 추가 - BE

--- src/test/java/bit/user/oauth/kakao/domain/KakaoUserInfoTest.java
+++ src/test/java/bit/user/oauth/kakao/domain/KakaoUserInfoTest.java
@@ -1,9 +1,9 @@
 package bit.user.oauth.kakao.domain;
 
+import static bit.user.oauth.kakao.fixture.JsonNodeTestFixture.createSampleJsonNode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.OffsetDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,30 +31,5 @@ class KakaoUserInfoTest {
         );
     }
 
-    private JsonNode createSampleJsonNode() throws Exception {
-        String jsonString = "{"
-                + "\"id\": 3663332253,"
-                + "\"connected_at\": \"2024-08-21T08:31:04Z\","
-                + "\"properties\": {"
-                + "\"nickname\": \"박준형\""
-                + "},"
-                + "\"kakao_account\": {"
-                + "\"profile_nickname_needs_agreement\": false,"
-                + "\"profile\": {"
-                + "\"nickname\": \"박준형\","
-                + "\"is_default_nickname\": false"
-                + "},"
-                + "\"has_email\": true,"
-                + "\"email_needs_agreement\": false,"
-                + "\"is_email_valid\": true,"
-                + "\"is_email_verified\": true,"
-                + "\"email\": \"pjh971212@gmail.com\""
-                + "}"
-                + "}";
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readTree(jsonString);
-
-    }
 
 }
