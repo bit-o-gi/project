fbed51060ba5e67c6f0f8fec4760874652a08ccf
ParkSeryu <pjh971212@gmail.com>
2024-09-05 18:39:12 +0900

    design: 화면 퍼블리싱 진행 - FE

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState} from '../store';
-import {toggleSidebar} from '../store/reducer/reducerSidebar';
-import {FaBars, FaTimes} from 'react-icons/fa';
-import tw from "tailwind-styled-components";
+import {openSidebar, closeSidebar} from '../store/reducer/reducerSidebar';
+import {
+    Drawer,
+    Button,
+    Typography,
+    IconButton,
+    List,
+    ListItem,
+    ListItemPrefix
+} from "@material-tailwind/react";
 
 const Sidebar = () => {
     const isSidebarOpen = useSelector((state: RootState) => state.sidebar.isOpen);
@@ -12,67 +19,89 @@ const Sidebar = () => {
 
     return (
         <>
-            <ToggleButton onClick={() => dispatch(toggleSidebar())}>
-                {isSidebarOpen ? <FaTimes/> : <FaBars/>}
-            </ToggleButton>
-            {isSidebarOpen && <Overlay onClick={() => dispatch(toggleSidebar())}/>}
+            <Button onClick={() => dispatch(openSidebar())}>Open Drawer</Button>
             {isSidebarOpen && (
-                <SidebarContainer>
-                    <StyledLink to="/">Home</StyledLink>
-                    <StyledLink to="/">Anniversary</StyledLink>
-                </SidebarContainer>
+                <Drawer open={isSidebarOpen} onClose={() => dispatch(closeSidebar())}>
+                    <div className="mb-2 flex items-center justify-between p-4">
+                        <Typography variant="h5" color="blue-gray">
+                            Menu
+                        </Typography>
+                        <IconButton variant="text" color="blue-gray" onClick={() => dispatch(closeSidebar())}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={2}
+                                stroke="currentColor"
+                                className="h-5 w-5"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </IconButton>
+                    </div>
+                    <List>
+                        <Link to={'/'}>
+                            <ListItem>
+                                <ListItemPrefix>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                        className="h-5 w-5"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </ListItemPrefix>
+                                Home
+                            </ListItem>
+                        </Link>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </ListItemPrefix>
+                            Anniversary
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </ListItemPrefix>
+                            Settings
+                        </ListItem>
+                    </List>
+                </Drawer>
             )}
         </>
     );
 };
-
-const ToggleButton = tw.button`
-    absolute
-    top-4
-    left-4
-    z-10
-    bg-blue-500
-    text-white
-    rounded-full
-    p-2
-    shadow-lg
-    hover:bg-blue-700
-    transition
-    duration-300
-`;
-
-const SidebarContainer = tw.div`
-    w-64
-    h-screen
-    bg-gray-800
-    text-white
-    flex
-    flex-col
-    items-center
-    pt-8
-    fixed
-    left-0
-    top-0
-    shadow-2xl
-    transition-transform
-    duration-300
-`;
-
-const StyledLink = tw(Link)`
-    my-4
-    text-xl
-    no-underline
-    hover:text-orange-500
-    transition
-    duration-300
-`;
-
-const Overlay = tw.div`
-    fixed
-    inset-0
-    bg-black
-    opacity-50
-    z-5
-`;
 
 export default Sidebar;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -4,17 +4,18 @@ import Sidebar from "../components/Sidebar";
 import {useDispatch} from 'react-redux';
 import {setAccessToken} from "../store/reducer/reducerUser";
 import tw from "tailwind-styled-components";
+import {AnniversaryCard} from "../components/AnniversaryCard";
+import {DdayCard} from "../components/DdayCard";
 
 const Main = () => {
-
     return (
         <MainContainer>
             <Sidebar/>
             <Content>
-                <Section>
-                    <Title>시작한지</Title>
-                    <Countdown>100일</Countdown>
-                </Section>
+                {/*<Section>*/}
+                <DdayCard/>
+                <AnniversaryCard/>
+                {/*</Section>*/}
             </Content>
         </MainContainer>
     );
@@ -23,24 +24,18 @@ const Main = () => {
 const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
-    background-color: #f0f4f8;
     min-height: 100vh;
 `;
 
 const Content = styled.div`
     flex: 1;
     display: flex;
+    background-color: #f9fafb;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 20px;
 `;
 
-const Title = tw.h1`
-    text-4xl
-    font-bold
-    p-4
-`;
 
 const Section = tw.section`
     mt-8
@@ -53,10 +48,5 @@ const Section = tw.section`
     w-full
 `;
 
-const Countdown = tw.h3`
-    text-2xl
-    font-semibold
-    text-pink-500
-`;
 
 export default Main;
