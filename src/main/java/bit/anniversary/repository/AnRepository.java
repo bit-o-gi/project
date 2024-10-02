e048fe94041f09455a2ed04e52357ab81c90629c
심봉교 <bonggyo.sim@ipageon.com>
2024-10-02 16:14:58 +0900

    feat : 기념일 기능 추가 - BE

--- src/main/java/bit/anniversary/repository/AnRepository.java
+++ src/main/java/bit/anniversary/repository/AnRepository.java
@@ -1,5 +1,8 @@
 package bit.anniversary.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,14 @@ import bit.anniversary.entity.Anniversary;
 
 @Repository
 public interface AnRepository extends JpaRepository<Anniversary,Long> {
+
+	// NOTE: 특정 날짜 범위 안에 있는 기념일들을 조회
+	List<Anniversary> findAllByAntimeBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+	// NOTE: 특정 날짜 이후의 기념일 조회
+	List<Anniversary> findAllByAntimeAfter(LocalDateTime date);
+
+	// NOTE: 특정 날짜 이전의 기념일 조회
+	List<Anniversary> findAllByAntimeBefore(LocalDateTime date);
+
 }
